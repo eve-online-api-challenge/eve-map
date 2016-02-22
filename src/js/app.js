f839d6f32a553041c3840eee332b150efefed055
Jack Bromley <phuhgh@gmail.com>
2016-02-22 18:37:06 +0000

    Fleshing out app a bit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
-(function(){
+(function () {
     'use strict';
-    
-    angular.module('fight-club', []);
-    
+
+    angular
+        .module('fight.club', ['ui.router'])
+        .config(['$stateProvider', '$urlRouterProvider', configureApp]);
+
+    function configureApp($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('index', {
+                url: '/',
+                noAuth: true,
+                templateUrl: '/templates/landing.html'
+            });
+    }
 })();
