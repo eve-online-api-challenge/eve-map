38723b35105f50b93ec8883dfbf46b7ab9578c05
Jack Bromley <phuhgh@gmail.com>
2016-02-26 23:03:01 +0000

    jumps by system, can't test because eve is offline >.<

--- jobs/jumps.job.js
+++ jobs/jumps.job.js
@@ -1,28 +1,49 @@
 "use strict";
 var https = require('https');
 var parseString = require('xml2js').parseString;
-var jumps = [];
-module.exports.getData = function getData() {
-    https.get('https://api.eveonline.com/map/Jumps.xml.aspx', handleResponse);
-};
+var schedule = require('node-schedule');
 
-function handleResponse(res) {
-    //TODO error handling
-    var doc = '';
-    res.on('data', function buildDoc(d) {
-        doc += d.toString();
-    })
-    res.on('end', function createObj() {
-        parseString(doc, function (err, obj) {
-            if (!err) addJumps(obj.eveapi.result[0].rowset[0].row);
-        });
-    })
-}
+function SystemJumpsJob(model) {
+    var job, self = this;
 
-function addJumps(systems) {
-    var i, system, l = systems.length;
-    for (i = 0; i < l; i++) {
-        system = systems[i].$;
-        jumps.push({ system: system.solarSystemID, jumps: system.shipJumps });
-    }
+    this.start = function () {
+        https.get('https://api.eveonline.com/map/Jumps.xml.aspx', handleResponse);
+    };
+
+    this.stop = function () {
+        job.cancel();
+    };
+
+    var handleResponse = function (res) {
+        var doc = '';
+        res.on('data', function buildDoc(d) {
+            doc += d.toString();
+        })
+        res.on('end', function createObj() {
+            parseString(doc, function (err, obj) {
+                var nextJobStartTime, jumps;
+                if (!err) {
+                    addJumps(jumps, obj.eveapi.result[0].rowset[0].row);
+                    model.insert(jumps, doNothing, doNothing);
+                    nextJobStartTime = new Date(obj.somepropertyidontknowbecauseeveisoffline);
+                }
+                else {
+                    //Try again in half an hour
+                    nextJobStartTime = new Date(Date.now() + 1800000);
+                }
+                //Either way, register the next job
+                job = schedule.scheduleJob(nextJobStartTime, self.start);
+            });
+        })
+    };
+
+    var addJumps = function (jumps, systems) {
+        var i, system, l = systems.length;
+        for (i = 0; i < l; i++) {
+            system = systems[i].$;
+            jumps.push({ system: system.solarSystemID, jumps: system.shipJumps });
+        }
+    };
+
+    var doNothing = function () { };
 }
