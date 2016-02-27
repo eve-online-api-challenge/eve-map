f3299801c9e0ecac08b44f2f30f42da86ad0b45a
Jack Bromley <phuhgh@gmail.com>
2016-02-27 18:20:26 +0000

    Playing with filters, some pretty huge performance issues on tree recursion so trying a level based approach, much faster but currently wrong answer...

--- src/js/universe/universe.factory.js
+++ src/js/universe/universe.factory.js
@@ -2,60 +2,174 @@
 
     angular
         .module('universe')
-        .factory('universeFactory', ['$http', universeFactory]);
+        .factory('universeFactory', ['universeHttpProvider', universeFactory]);
 
-    function universeFactory($http) {
-        var kspace = {};
-        var subspace = {};
-        var limits = { xMin: 0, xMax: 0, yMin: 0, yMax: 0, zMin: 0, zMax: 0 }
-        function getKspace() {
-            // var promise = $http.jsonp('/sde/kspace.json');
-            var promise = $http.get('/sde/kspace.json');
-            promise.success(function gotKspace(data) {
-                angular.copy(data, kspace);
-                //Apply no filters as standard
-                filter();
+    function universeFactory(provider) {
+        //Initialization, start with all systems
+        var systems = {};
+        var limits = { xMin: 0, xMax: 0, yMin: 0, yMax: 0, zMin: 0, zMax: 0 };
+
+        function initialize() {
+            return provider.getKspace().then(function () {
+                angular.copy(provider.systems, systems);
+                updateSubsetLimits();
             });
-            return promise;
         }
-        function filter(filters) {
-            //TODO think of filter schema
-            //Apply filters to get subspace
-            if (!filters)
-                angular.copy(kspace, subspace);
-            //Adjust map limits
-            setMapLimits();
-        }
-        function setMapLimits() {
+
+        function updateSubsetLimits() {
             limits.xMin = 0, limits.xMax = 0;
             limits.yMin = 0, limits.yMax = 0;
             limits.zMin = 0, limits.zMax = 0;
-            var keys = Object.keys(kspace);
+
+            var keys = Object.keys(systems);
             var i, l = keys.length;
+
             for (i = 0; i < l; i++) {
-                var system = kspace[keys[i]];
-                updateLimits(system.x, system.y, system.z);
+                var system = systems[keys[i]];
+
+                if (system.x > limits.xMax)
+                    limits.xMax = system.x;
+                else if (system.x < limits.xMin)
+                    limits.xMin = system.x;
+                if (system.y > limits.yMax)
+                    limits.yMax = system.y;
+                else if (system.y < limits.yMin)
+                    limits.yMin = system.y;
+                if (system.z > limits.zMax)
+                    limits.zMax = system.z;
+                else if (system.z < limits.zMin)
+                    limits.zMin = system.z;
             }
         }
-        function updateLimits(x, y, z) {
-            if (x > limits.xMax)
-                limits.xMax = x;
-            else if (x < limits.xMin)
-                limits.xMin = x;
-            if (y > limits.yMax)
-                limits.yMax = y;
-            else if (y < limits.yMin)
-                limits.yMin = y;
-            if (z > limits.zMax)
-                limits.zMax = z;
-            else if (z < limits.zMin)
-                limits.zMin = z;
+
+        var filter = {
+            byJumpsFromSystem: filterByJumpsFromSystem,
+            byRegion: filterByRegion,
+            bySecurityGreaterThan: filterSecurityGreaterThan,
+            bySecurityLessThan: filterSecurityLessThan
         }
+
+        //         function filterByJumpsFromSystem(startId, n) {
+        //             //want to be able to chain filters, because that would be cool...
+        //             var subset = {};
+        //             recurse(startId, undefined, 0);
+        // 
+        //             //Need to recurse a tree to depth n and
+        //             //return all systems in the tree
+        //             function recurse(systemId, previousSystemId, depth) {
+        //                 var system = systems[systemId];
+        //                 if (depth < n) {
+        //                     system.connections.forEach(function (connectionId) {
+        //                         subset[connectionId] = systems[connectionId];
+        //                         
+        //                         if (previousSystemId !== connectionId)
+        //                             recurse(connectionId, systemId, depth + 1);
+        //                     });
+        //                 }
+        //             }
+        //             angular.copy(subset, systems);
+        //         }
+
+        function filterByJumpsFromSystem(startId, n) {
+            //Experiment to optimize tree depth testing
+            var layers = [[startId]];
+            goDeeper();
+            
+            var tot = 0;
+            layers.forEach(function(layer) {
+                layer.forEach(function() {
+                   tot++; 
+                });
+            });
+            console.log('total systems in unvirse: ', tot);
+            var keys = Object.keys(systems);
+            console.log('we expected: ', keys.length)
+            //Quack quack, there aren't enough systems in the unvierse...
+            
+            function goDeeper() {
+                //Add new layer for the next set of systems
+                layers.push([]);
+                var previousLayer = layers[layers.length - 2];
+                var currentLayer = layers[layers.length - 1];
+
+                previousLayer.forEach(function (systemId) {
+                    var system = systems[systemId];
+                    system.connections.forEach(function (connectionId) {
+                        if (testNotExist(connectionId))
+                            currentLayer.push(connectionId);
+                    });
+                });
+                if (layers.length < n)
+                    goDeeper();
+
+            }
+            function testNotExist(systemId) {
+                var l = layers.length;
+
+                //Reverse order testing should be more efficient generally
+                while (l--) {
+                    var k = layers[l].length;
+                    while (k--) {
+                        if (systemId === layers[l][k])
+                            return false;
+                    }
+                }
+                
+                //The system is new
+                return true;
+            }
+        }
+
+
+        function filterByRegion(region) {
+
+        }
+
+        function filterSecurityGreaterThan(sec) {
+
+        }
+
+        function filterSecurityLessThan(sec) {
+
+        }
+
         return {
-            "get": getKspace,
-            "systems": subspace,
-            "limits": limits,
-            "filter": filter
+            systems: systems,
+            limits: limits,
+            initialize: initialize,
+            filter: filter
+        }
+    }
+
+    //want something injectable
+    angular
+        .module('universe')
+        .factory('universeHttpProvider', ['$http', universeHttpProvider]);
+
+    function universeHttpProvider($http) {
+        var systems = {};
+        var jumps = {};
+
+        function getKspace() {
+            return $http.get('/sde/kspace.json')
+                .success(function gotKspace(data) {
+                    angular.copy(data, systems);
+                });
+        }
+
+        function getJumps() {
+            var promise = $http.get('/universe/jumps/latest');
+            promise.success(function gotKspace(data) {
+                angular.copy(data.jumps, jumps);
+            });
+            return promise;
+        }
+
+        return {
+            systems: systems,
+            jumps: jumps,
+            getKspace: getKspace,
+            getJumps: getJumps
         };
     }
 
--- src/js/universe/universe.map.ctrl.js
+++ src/js/universe/universe.map.ctrl.js
@@ -2,35 +2,20 @@
 
     angular
         .module('universe')
-        .controller('UniverseMapCtrl', ['universeFactory', function (universe) {
-            var promise = universe.get();
-            promise.success(function () {
-                // universe = {
-                //     systems: {
-                //         '1': { x: 0, y: 0, z: 0 },
-                //         '2': { x: 0, y: 2, z: 3 },
-                //         '3': { x: 0, y: 3, z: -3 },
-                //         '4': { x: 3, y: 4, z: 0 },
-                //         '5': { x: 3, y: 5, z: 3 },
-                //         '6': { x: 3, y: 6, z: -3 },
-                //         '7': { x: -3, y: 7, z: 0 },
-                //         '8': { x: -3, y: 8, z: 3 },
-                //         '9': { x: -3, y: 9, z: -3 },
-                //     },
-                //     limits: {
-                //         xMin: -3,
-                //         xMax: 3,
-                //         yMin: -1,
-                //         yMax: 9,
-                //         zMin: -3,
-                //         zMax: 3
-                //     }
-                // };
-                var universeMap = new UniverseMap(universe);
-                universeMap.initialize();
-                universeMap.render();
-            });
+        .controller('UniverseMapCtrl', ['universeFactory', UniverseMapCtrl]);
 
-        }]);
+    function UniverseMapCtrl(universe) {
+        universe
+            .initialize()
+            .then(createMap);
+
+        function createMap() {
+            universe.filter.byJumpsFromSystem('30000178', 100);
+
+            var universeMap = new UniverseMap(universe);
+            universeMap.initialize();
+            universeMap.render();
+        }
+    }
 
 })();
